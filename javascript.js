9b42461d630eccff8950f0d661f077f2b587c50f
dkapper01 <dkapper01@gmail.com>
2017-09-04 17:38:47 -0500

    key down and up. Draw paddle

--- javascript.js
+++ javascript.js
@@ -7,6 +7,14 @@ var dx = 2;
 var dy = -2; 
 var ballRadius = 10;
 
+var paddleHeight = 10; 
+var paddleWidth = 75; 
+
+var paddleX = (canvas.width-paddleWidth)/2; 
+
+var rightPressed = false; 
+var leftPressed = false; 
+
 function drawBall() {
 	ctx.beginPath(); 
 	ctx.arc(x, y, ballRadius, 0, Math.PI*2); 
@@ -15,9 +23,18 @@ function drawBall() {
 	ctx.closePath(); 
 }
 
+function drawPaddle() {
+	ctx.beginPath();
+	ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight); 
+	ctx.fillStyle = "#0095DD"; 
+	ctx.fill();
+	ctx.closePath();
+}
+
 function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height); 
 	drawBall(); 
+	drawPaddle();
 	x += dx;
 	y += dy; 
 
@@ -28,6 +45,22 @@ function draw() {
 	if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
 	    dy = -dy;
 	}
+}
+document.addEventListener("keydown", keyDownHandler, false); 
+document.addEventListener("keyup", keyUpHandler, false); 
+
+function keyDownHandler(event) {
+	if(event.keyCode == 39) {
+		rightPressed = true; 
+	} else if(event.keyCode == 37) {
+		leftPressed = true; 
+	}
+}
+function keyUpHandler(event) {
+	if(event.keyCode == 37) {
+		rightPressed = false; 
+	} else if(event.keyCode == 39) {
+		rightPressed = false; 
 }
 setInterval(draw, 10);
 
