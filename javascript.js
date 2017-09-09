87048ee8aa0602ab6e6e5e7664d7885aec2c83eb
dkapper01 <dkapper01@gmail.com>
2017-09-08 22:14:19 -0500

    ball bounces off paddle

--- javascript.js
+++ javascript.js
@@ -49,8 +49,15 @@ function draw() {
     	dx = -dx;
 	}
 
-	if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+	if(y + dy < ballRadius) {
 	    dy = -dy;
+	} else if(y + dy > canvas.height-ballRadius) {
+		if(x > paddleX && x < paddleX + paddleWidth) {
+			dy = -dy;
+		} else {
+			alert("Game"); 
+			document.location.reload(); 
+		}
 	}
 }
 document.addEventListener("keydown", keyDownHandler, false); 
